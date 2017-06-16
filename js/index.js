5ce9d888748d7c26436129ac3e3bca22fcc837c4
Dean <jmdean1@pstcc.edu>
2017-06-16 10:21:05 -0400

    some animations

--- js/index.js
+++ js/index.js
@@ -11,57 +11,57 @@ app.controller("myCtrl", function ($scope) {
             links: [
               {
                 text: "Statistics Thing 1",
-                url: "https://google.com",
+                url: "http://http://google.com",
                 // desc:
                 // "Some information and stuff for link 1 in section 1 in unit 1. The next button doesn't have a description",
                 icon: "fa-bath"
               },
               {
                 text: "Statistics Thing 2",
-                url: "https://yahoo.com",
+                url: "http://yahoo.com",
                 icon: "fa-camera-retro"
               }
             ]
           },
           {
-            title: "Section 1.2",
+            title: "Section 2.12",
             links: [
               {
                 text: "Statistics Thing 2.1",
-                url: "https://google.com",
+                url: "http://google.com",
                 icon: "fa-wpexplorer"
               },
               {
                 text: "Statistics Thing 2",
-                url: "https://yahoo.com",
+                url: "http://yahoo.com",
                 desc:
                 "Some information and stuff 2 for link 2 in unit 1 and section 2",
                 icon: "fa-telegram"
               },
               {
                 text: "Statistics Thing 2.1",
-                url: "https://google.com",
+                url: "http://google.com",
                 desc:
                 "Some information and stuff for link 1 in section 2 in unit 1",
                 icon: "fa-ravelry"
               },
               {
                 text: "Statistics Thing 2",
-                url: "https://yahoo.com",
+                url: "http://yahoo.com",
                 desc:
                 "Some information and stuff 2 for link 2 in unit 1 and section 2",
                 icon: "fa-briefcase"
               },
               {
                 text: "Statistics Thing 2.1",
-                url: "https://google.com",
+                url: "http://google.com",
                 desc:
                 "Some information and stuff for link 1 in section 2 in unit 1",
                 icon: "fa-cogs"
               },
               {
                 text: "Statistics Thing 2",
-                url: "https://yahoo.com",
+                url: "http://yahoo.com",
                 desc:
                 "Some information and stuff 2 for link 2 in unit 1 and section 2",
                 icon: "fa-database"
@@ -78,40 +78,40 @@ app.controller("myCtrl", function ($scope) {
           links: [
             {
               text: "Statistics Thing 2.1",
-              url: "https://google.com",
+              url: "http://google.com",
               icon: "fa-wpexplorer"
             },
             {
               text: "Statistics Thing 2",
-              url: "https://yahoo.com",
+              url: "http://yahoo.com",
               desc:
               "Some information and stuff 2 for link 2 in unit 1 and section 2",
               icon: "fa-telegram"
             },
             {
               text: "Statistics Thing 2.1",
-              url: "https://google.com",
+              url: "http://google.com",
               desc:
               "Some information and stuff for link 1 in section 2 in unit 1",
               icon: "fa-ravelry"
             },
             {
               text: "Statistics Thing 2",
-              url: "https://yahoo.com",
+              url: "http://yahoo.com",
               desc:
               "Some information and stuff 2 for link 2 in unit 1 and section 2",
               icon: "fa-briefcase"
             },
             {
               text: "Statistics Thing 2.1",
-              url: "https://google.com",
+              url: "http://google.com",
               desc:
               "Some information and stuff for link 1 in section 2 in unit 1",
               icon: "fa-cogs"
             },
             {
               text: "Statistics Thing 2",
-              url: "https://yahoo.com",
+              url: "http://yahoo.com",
               desc:
               "Some information and stuff 2 for link 2 in unit 1 and section 2",
               icon: "fa-database"
@@ -134,7 +134,7 @@ app.controller("myCtrl", function ($scope) {
           links: [
             {
               text: "Statistics Thing 1",
-              url: "http://https://google.com",
+              url: "http://http://google.com",
               desc:
               "Some information and stuff ",
               icon: "fa-bath"
